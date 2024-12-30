c8dba8badeeaf0ef546eef725db8072addad7fde
Nceba-Developer <143310721+Nceba-Developer@users.noreply.github.com>
2024-12-30 20:00:55 +0200

    updated

--- FNN_regression.py
+++ FNN_regression.py
@@ -15,15 +15,4 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# feature selection
-# Ordinary Least Squares - the technique for fitting a linear regression model by minimizing the sum of squared residuals (errors)
-# Ordinary Least Squares - used in regression analysis to estimate the parameters of a linear model. Its goal is to minimize the differences between observed values and predicted values (i.e., residuals).
-
-X_tr, X_ts, y_tr, y_ts = train_test_split(predictors_col, target_col, test_size=0.2, random_state=42)
-lr = LinearRegression()
-lr.fit(X_tr, y_tr)
-X_tr_const = api.add_constant(X_tr)
-model_OLS = api.OLS(y_train, X_tr_const).fit()
-# print(model_OLS.summary()) # FNN_regression
-
 model = Sequential()
--- feature_selection.py
+++ feature_selection.py
@@ -7,6 +7,9 @@ from statsmodels import api
 predictors_col = customerloan.drop(columns=["approval_date", "loan_amount"])
 target_col = customerloan["loan_amount"]
 
+# feature selection
+# Ordinary Least Squares - the technique for fitting a linear regression model by minimizing the sum of squared residuals (errors)
+# Ordinary Least Squares - used in regression analysis to estimate the parameters of a linear model. Its goal is to minimize the differences between observed values and predicted values (i.e., residuals).
 
 X_tr, X_ts, y_tr, y_ts = train_test_split(predictors_col, target_col, test_size=0.2, random_state=42)
 lr = LinearRegression()
